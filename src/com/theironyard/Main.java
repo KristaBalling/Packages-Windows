8265f7a8e942ac524bd99f07c3b068873e36180e
Krista Blachian <krista.blachian@scifusion360.com>
2018-11-27 16:01:44 -0500

    exercise complete

---
+++ src/com/theironyard/Main.java
@@ -0,0 +1,9 @@
+package com.theironyard;
+
+public class Main {
+
+    public static void main(String[] args) {
+	MyWindow myWindow = new MyWindow("Java Window");
+	myWindow.setVisible(true);
+    }
+}
